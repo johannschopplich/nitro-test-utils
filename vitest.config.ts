652f00ac4788cba8e45e35292613a62607daa947
Johann Schopplich <mail@johannschopplich.com>
2024-08-29 11:54:15 +0200

    test: simplify Vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,4 @@
 // eslint-disable-next-line antfu/no-import-dist
 import { defineConfig } from './dist/config.mjs'
 
-export default defineConfig({
-  nitro: {
-    rerunOnSourceChanges: true,
-  },
-})
+export default defineConfig()
