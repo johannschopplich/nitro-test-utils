f8ffb8df48afb181f793858f6684ea793ff4407f
Johann Schopplich <mail@johannschopplich.com>
2026-03-06 07:43:03 +0100

    chore: update ESLint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,9 +1,13 @@
+import type { ConfigNames, TypedFlatConfigItem } from '@antfu/eslint-config'
+import type { FlatConfigComposer } from 'eslint-flat-config-utils'
 import antfu from '@antfu/eslint-config'
 
-export default antfu({
+const config: FlatConfigComposer<TypedFlatConfigItem, ConfigNames> = antfu({
   ignores: ['**/.nitro/**'],
 }).append({
   rules: {
     'antfu/no-top-level-await': 'off',
   },
 })
+
+export default config
