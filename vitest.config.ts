8efcae5cbcc34530f8d081152e0e9cad6bfc656c
Johann Schopplich <mail@johannschopplich.com>
2026-03-13 08:48:08 +0100

    test: update vitest config to specify test directory

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,10 @@ import type { UserConfig } from 'vite'
 // eslint-disable-next-line antfu/no-import-dist
 import { defineConfig } from './dist/config.mjs'
 
-const config: UserConfig = await defineConfig()
+const config: UserConfig = await defineConfig({
+  test: {
+    dir: './test',
+  },
+})
 
 export default config
